06a29bd21be2acc20a1c33d36d3f48d385d157e9
Tuấn Trần <juriyaro@gmail.com>
2017-02-02 12:57:41 +0700

    First API

---
+++ API/Web Service/App_Start/WebApiConfig.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace Web_Service
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            var cors = new EnableCorsAttribute("*", "*", "*");
+            config.EnableCors(cors);
+
+            //config.EnableCors(new EnableCorsAttribute("http://localhost:1188", "*", "*"));
+
+            config.Routes.MapHttpRoute(
+               name: "ActionApi",
+               routeTemplate: "api/{controller}/{action}/{id}"
+            );
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            var json = config.Formatters.JsonFormatter;
+            json.SerializerSettings.PreserveReferencesHandling = Newtonsoft.Json.PreserveReferencesHandling.Objects;
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
+        }
+    }
+}
